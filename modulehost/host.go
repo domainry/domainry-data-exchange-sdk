4eaa07d8b0bf61d116057c2a8dc77f094a6829a7
panbin <panbin@codeck.ai>
2026-09-23 19:10:11 +0800

    refactor(modulehost): stop injecting artifact store

--- modulehost/host.go
+++ modulehost/host.go
@@ -5,7 +5,6 @@ import (
 	"database/sql"
 
 	dataexchange "github.com/domainry/domainry-data-exchange-sdk"
-	sharedartifact "github.com/domainry/domainry-foundation/artifact"
 )
 
 type Host interface {
@@ -16,7 +15,6 @@ type Host interface {
 type ModuleHost interface {
 	Host
 	Database() *sql.DB
-	ArtifactStore() sharedartifact.Store
 	Migrations() MigrationRegistrar
 	WorkspaceContext(context.Context, string, string) context.Context
 }
